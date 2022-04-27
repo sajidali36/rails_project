8e3ec7641c6c936f29049482568f1c5f444bf895
sajidali36 <sajidali.362537@gmail.com>
2022-04-27 12:26:21 +0500

    User model for devise is created with custom fields

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  has_many :projects_users
+  has_many :projects, through: :projects_users
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+end
