67f2ba57c62f086bb61a27d7ad2a0807e4c9ea09
sajidali36 <sajidali.362537@gmail.com>
2022-04-27 14:10:14 +0500

    rubocop error removed from routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   resources :bugs
   root 'projects#index'
   resources :projects
   devise_for :users
-
-
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
