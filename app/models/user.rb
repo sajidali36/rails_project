bbccbb04ba0909439c99a9aec2473bcb382b3e3a
sajidali36 <sajidali.362537@gmail.com>
2022-04-27 13:48:01 +0500

    robo errors remove from user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,16 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   has_many :projects_users
   has_many :projects, through: :projects_users
+
+  has_many :bugs_users
+  has_many :bugs, through: :bugs_users
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable,
+    :registerable,
+    :recoverable,
+    :rememberable,
+    :validatable
 end
