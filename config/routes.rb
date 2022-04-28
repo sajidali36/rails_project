116da6a4e90e1e14f4e7eed82d2ab6816a3012a1
sajidali36 <sajidali.362537@gmail.com>
2022-04-28 11:23:40 +0500

    Fronend designing done of sign up and sign in pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :bugs
-  root 'projects#index'
-  resources :projects
   devise_for :users
+  resources :bugs
+  resources :projects
+
+  devise_scope :user do
+    authenticated :user do
+      root 'projects#index', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
 end
