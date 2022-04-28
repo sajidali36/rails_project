a452a650ed5408bcc725183aa741555be2282635
sajidali36 <sajidali.362537@gmail.com>
2022-04-28 17:37:26 +0500

    creating and deleting project functionality added to manager ddashboard

--- app/controllers/dashboard_developers_controller.rb
+++ app/controllers/dashboard_developers_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class DashboardDevelopersController < ApplicationController
   def index
   end
--- app/controllers/dashboard_managers_controller.rb
+++ app/controllers/dashboard_managers_controller.rb
@@ -1,16 +1,24 @@
+# frozen_string_literal: true
+
 class DashboardManagersController < ApplicationController
+  before_action :set_project, only: %i[edit]
   def index
+    @projects = Project.all
   end
 
   def show
+    @projects = Project.all
   end
 
-  def edit
-  end
+  def edit; end
 
-  def new
-  end
+  def new; end
 
-  def create
+  def create; end
+
+  private
+
+  def set_project
+    @project = Project.find(params[:id])
   end
 end
