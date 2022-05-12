10e08c25415d8c9ae6b168a2d69aed66fcca8927
sajidali36 <sajidali.362537@gmail.com>
2022-05-12 20:26:40 +0500

    bootstrap added

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,10 +8,30 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+require("@popperjs/core")
 
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
+import "bootstrap"
+
+// Import the specific modules you may need (Modal, Alert, etc)
+import { Tooltip, Popover } from "bootstrap"
+
+// The stylesheet location we created earlier
+require("../stylesheets/application.scss")
+
+// If you're using Turbolinks. Otherwise simply use: jQuery(function () {
+document.addEventListener("turbolinks:load", () => {
+  // Both of these are from the Bootstrap 5 docs
+  var tooltipTriggerList = [].slice.call(
+    document.querySelectorAll('[data-bs-toggle="tooltip"]')
+  )
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new Tooltip(tooltipTriggerEl)
+  })
+
+  var popoverTriggerList = [].slice.call(
+    document.querySelectorAll('[data-bs-toggle="popover"]')
+  )
+  var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new Popover(popoverTriggerEl)
+  })
+})
