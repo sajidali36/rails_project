e95a5c8715d77f22e8c0e0796ab215132a48cabd
sajidali36 <sajidali.362537@gmail.com>
2022-05-16 17:42:19 +0500

    when new bug is added emails are sent to developers

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,12 +1,16 @@
 # frozen_string_literal: true
 
 class UserMailer < ApplicationMailer
-  default from: 'notifications@example.com'
+  default from: 'sajid.forsendemail@gmail.com'
 
   def welcome_email
     # @user = params[:user]
     @user = User.find(4)
     @url = 'https://www.w3schools.com/'
     mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+  end
+
+  def bug_created(user)
+    mail(to: user.email, subject: 'new Bug is created')
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -13,14 +13,13 @@ class User < ApplicationRecord
     qa: 2
   }
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable,
     :registerable,
     :recoverable,
     :rememberable,
     :validatable,
-    :confirmable,
-    :async
+    :confirmable
 
   def send_devise_notification(notification, *args)
     devise_mailer.send(notification, self, *args).deliver_later
